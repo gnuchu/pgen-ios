dabfcc27f2506403792e1505aa3827fff92f6ade
John Griffiths <gnuchu@gmail.com>
2015-08-30 21:17:35 +0100

    Initial Commit

---
+++ pgen-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pgen-ios
+//
+//  Created by John Griffiths on 30/08/2015.
+//  Copyright (c) 2015 John Griffiths. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
